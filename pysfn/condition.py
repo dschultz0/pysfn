f64c2bd259e85994609b606a92048a43905f15bf
Dave Schultz <dave@daveschultzconsulting.com>
2023-01-04 16:59:21 -0800

    Expanded condition options

--- pysfn/condition.py
+++ pysfn/condition.py
@@ -1,5 +1,16 @@
 import ast
 from aws_cdk import aws_stepfunctions as sfn
+from typing import Dict, Tuple, Callable
+
+# Limited comparisons types for now...
+comparator_map: Dict[Tuple] = {
+    (ast.Eq, str): (sfn.Condition.string_equals, "=="),
+    (ast.Eq, int): (sfn.Condition.number_equals, "=="),
+    (ast.Gt, int): (sfn.Condition.number_greater_than, ">"),
+    (ast.Gt, float): (sfn.Condition.number_greater_than, ">"),
+    (ast.Lt, int): (sfn.Condition.number_less_than, "<"),
+    (ast.Lt, float): (sfn.Condition.number_less_than, "<"),
+}
 
 
 def build_condition(test):
@@ -9,21 +20,41 @@ def build_condition(test):
             if_value(test.id, None),
             f"If {test.id}",
         )
+    elif (
+        isinstance(test, ast.Subscript)
+        and isinstance(test.value, ast.Name)
+        and isinstance(test.slice, ast.Constant)
+    ):
+        var_name = f"{test.value.id}.{test.slice.value}"
+        return (
+            if_value(var_name, None),
+            f"If {var_name}",
+        )
     elif isinstance(test, ast.Compare):
-        # Just going to handle simple comparison to string values for now
-        if (
-            isinstance(test.left, ast.Name)
-            and len(test.ops) == 1
-            and isinstance(test.ops[0], ast.Eq)
-            and len(test.comparators) == 1
-            and isinstance(test.comparators[0], ast.Constant)
+        # Assuming that the variable is on the left side as well
+        var_name = None
+        if isinstance(test.left, ast.Name):
+            var_name = test.left.id
+        elif (
+            isinstance(test.left, ast.Subscript)
+            and isinstance(test.left.value, ast.Name)
+            and isinstance(test.left.slice, ast.Constant)
         ):
-            return (
-                sfn.Condition.string_equals(
-                    f"$.register.{test.left.id}", test.comparators[0].value
-                ),
-                f"If {test.left.id}=='{test.comparators[0].value}'",
-            )
+            var_name = f"{test.left.value.id}.{test.left.slice.value}"
+        if var_name and len(test.ops) == 1 and len(test.comparators) == 1:
+            comparator = test.comparators[0]
+            if isinstance(comparator, ast.Constant):
+                comp_op: Callable
+                label: str
+                comp_op, label = comparator_map.get(
+                    (type(test.ops[0]), type(comparator.value)), (None, None)
+                )
+                if comp_op:
+                    return (
+                        comp_op(f"$.register.{var_name}", comparator.value),
+                        f"If {var_name}{label}'{comparator.value}'",
+                    )
+
     raise Exception(f"Unhandled test: {ast.dump(test)}")
 
 
